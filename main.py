1774ba718a24e9669575dcb210615390e2f06592
Edu <serranotrenado.e@gmail.com>
2025-10-28 01:04:07 +0100

    learning docs

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from typing import Union
-
 from fastapi import FastAPI
 
-app = FastAPI()
+# Objeto app de tipo FastApi
+app = FastAPI(
+    title="FoodAPI",
+    version="0.0.2"
+)
 
+# Configuracion del api restful
 
+# Endpoint GET /
 @app.get("/")
 def read_root():
     return {"Hello": "Edu"}
 
+# Endpoint GET /ingredientes
+@app.get("/ingredientes")
+def read_ingredientes():
+    return {"Objeto": "Ingredientes"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
